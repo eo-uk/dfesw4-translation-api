ec88fd9a609f1900398c5dea97d3c706b8cf7e84
eo-uk <london.ekin@gmail.com>
2021-11-23 15:51:10 +0000

    added wordpair controller get mappings

--- DFESW4-TranslationAPI/src/main/java/com/qa/dfesw4translationapi/controllers/WordPairController.java
+++ DFESW4-TranslationAPI/src/main/java/com/qa/dfesw4translationapi/controllers/WordPairController.java
@@ -1,9 +1,14 @@
 package com.qa.dfesw4translationapi.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.qa.dfesw4translationapi.entities.WordPair;
 import com.qa.dfesw4translationapi.services.WordPairService;
 
 
@@ -13,5 +18,30 @@ public class WordPairController {
 	
 	@Autowired
 	private WordPairService service;
+	
+	@GetMapping("/words")
+	public List<WordPair> getAllWords() {
+		return service.getAllWords();
+	}
+	
+	@GetMapping("/words/field/{field}")
+	public List<WordPair> getWordsByField (@PathVariable String field){
+		return service.getWordsByField(field);
+	}
+	
+	@GetMapping("/words/id/{id}")
+	public WordPair getWordById(@PathVariable int id) {
+		return service.getWordById(id);
+	}
+	
+	@GetMapping("/words/word/{word}")
+	public List<WordPair> getWordsByWord(@PathVariable String word) {
+		return service.getWordsByWord(word);
+	}
+	
+	@GetMapping("/words/language/{language}")
+	public List<WordPair> getWordsByLanguage(@PathVariable String language) {
+		return service.getWordsByLanguage(language);
+	}
 	
 }
