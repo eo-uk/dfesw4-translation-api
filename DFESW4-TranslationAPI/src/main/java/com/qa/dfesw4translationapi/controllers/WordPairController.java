9286ada3defc74ffddb82de8131cc79cc485e84c
eo-uk <london.ekin@gmail.com>
2021-11-23 15:52:38 +0000

    added wordpair controller post, put, delete mappings

--- DFESW4-TranslationAPI/src/main/java/com/qa/dfesw4translationapi/controllers/WordPairController.java
+++ DFESW4-TranslationAPI/src/main/java/com/qa/dfesw4translationapi/controllers/WordPairController.java
@@ -1,10 +1,15 @@
 package com.qa.dfesw4translationapi.controllers;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,6 +47,35 @@ public class WordPairController {
 	@GetMapping("/words/language/{language}")
 	public List<WordPair> getWordsByLanguage(@PathVariable String language) {
 		return service.getWordsByLanguage(language);
+	}
+	
+	@PutMapping("/words/update/{id}")
+	public HashMap<String, String> updateWord(
+			@RequestBody WordPair newWordPair,
+			@PathVariable int id
+	) {
+		service.updateWord(newWordPair, id);
+		HashMap<String, String> response = new HashMap<String, String>();
+		response.put("message", "success");
+		return response;
+	}
+	
+	@PostMapping("/words/add")
+	public HashMap<String, String> addWord(@RequestBody WordPair word) {
+		service.createWord(word);
+		HashMap<String, String> response = new HashMap<String, String>();
+		response.put("message", "success");
+	    return response;
+	}
+	
+	@DeleteMapping("/words/delete/{id}")
+	void deleteWordById(@PathVariable int id) {
+		service.deleteWordById(id);
+	}
+	
+	@DeleteMapping("/words/delete/all")
+	void deleteAllWords() {
+		service.deleteAllWords();
 	}
 	
 }
