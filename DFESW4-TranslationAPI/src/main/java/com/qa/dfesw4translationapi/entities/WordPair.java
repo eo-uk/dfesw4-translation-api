d39299b65f0247f3c841f4ff20e6644038e9758e
eo-uk <london.ekin@gmail.com>
2021-11-23 12:35:50 +0000

    added wordpair entity getters, setters, toString

--- DFESW4-TranslationAPI/src/main/java/com/qa/dfesw4translationapi/entities/WordPair.java
+++ DFESW4-TranslationAPI/src/main/java/com/qa/dfesw4translationapi/entities/WordPair.java
@@ -49,4 +49,66 @@ public class WordPair {
 		this.field = field;
 		this.dateCreated = dateCreated;
 	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLanguage1() {
+		return language1;
+	}
+
+	public void setLanguage1(String language1) {
+		this.language1 = language1;
+	}
+
+	public String getLanguage1Word() {
+		return language1Word;
+	}
+
+	public void setLanguage1Word(String language1Word) {
+		this.language1Word = language1Word;
+	}
+
+	public String getLanguage2() {
+		return language2;
+	}
+
+	public void setLanguage2(String language2) {
+		this.language2 = language2;
+	}
+
+	public String getLanguage2Word() {
+		return language2Word;
+	}
+
+	public void setLanguage2Word(String language2Word) {
+		this.language2Word = language2Word;
+	}
+
+	public String getField() {
+		return field;
+	}
+
+	public void setField(String field) {
+		this.field = field;
+	}
+	
+	public String getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(String dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	@Override
+	public String toString() {
+		return "WordPair [id=" + id + ", language1=" + language1 + ", language1Word=" + language1Word + ", language2="
+				+ language2 + ", language2Word=" + language2Word + "]";
+	}
 }
