f5a9ef986852dcceada8f9f1e58a33f494378b78
eo-uk <london.ekin@gmail.com>
2021-11-23 15:47:38 +0000

    added wordpair controller service

--- DFESW4-TranslationAPI/src/main/java/com/qa/dfesw4translationapi/controllers/WordPairController.java
+++ DFESW4-TranslationAPI/src/main/java/com/qa/dfesw4translationapi/controllers/WordPairController.java
@@ -1,5 +1,17 @@
 package com.qa.dfesw4translationapi.controllers;
 
-public class WordPairController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.qa.dfesw4translationapi.services.WordPairService;
+
+
+@RestController
+@RequestMapping("/api")
+public class WordPairController {
+	
+	@Autowired
+	private WordPairService service;
+	
 }
