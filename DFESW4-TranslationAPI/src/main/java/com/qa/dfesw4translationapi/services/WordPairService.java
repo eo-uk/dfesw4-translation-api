8cfc2c984dcd4dcaae8d4d3876198ffdb931ebd5
eo-uk <london.ekin@gmail.com>
2021-11-23 15:30:07 +0000

    added wordpair service repo

--- DFESW4-TranslationAPI/src/main/java/com/qa/dfesw4translationapi/services/WordPairService.java
+++ DFESW4-TranslationAPI/src/main/java/com/qa/dfesw4translationapi/services/WordPairService.java
@@ -1,5 +1,15 @@
 package com.qa.dfesw4translationapi.services;
 
-public class WordPairService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.qa.dfesw4translationapi.entities.WordPairRepository;
+
+
+@Service
+public class WordPairService {
+	
+	@Autowired
+	WordPairRepository repo;
+	
 }
