c5a150c914d2186d359f0ca08522dbc8f62f500f
eo-uk <london.ekin@gmail.com>
2021-11-23 12:30:43 +0000

    initial

---
+++ DFESW4-TranslationAPI/src/main/java/com/qa/dfesw4translationapi/main/Dfesw4TranslationApiApplication.java
@@ -0,0 +1,13 @@
+package com.qa.dfesw4translationapi.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dfesw4TranslationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dfesw4TranslationApiApplication.class, args);
+	}
+
+}
---
+++ DFESW4-TranslationAPI/src/test/java/com/qa/dfesw4translationapi/main/Dfesw4TranslationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.dfesw4translationapi.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dfesw4TranslationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
