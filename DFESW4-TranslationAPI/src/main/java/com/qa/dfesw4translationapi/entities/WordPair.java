b6e0f17c4f8b0fe298513c378272273b0c31db65
eo-uk <london.ekin@gmail.com>
2021-11-23 12:34:43 +0000

    added wordpair entity fields

--- DFESW4-TranslationAPI/src/main/java/com/qa/dfesw4translationapi/entities/WordPair.java
+++ DFESW4-TranslationAPI/src/main/java/com/qa/dfesw4translationapi/entities/WordPair.java
@@ -1,5 +1,37 @@
 package com.qa.dfesw4translationapi.entities;
 
-public class WordPair {
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class WordPair {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column
+	private String language1;
+	
+	@Column
+	private String language1Word;
+	
+	@Column
+	private String language2;
+	
+	@Column
+	private String language2Word;
+	
+	@Column
+	private String field = "General";
+	
+	@Column
+	private String dateCreated = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+	
 }
