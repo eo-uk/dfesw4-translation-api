26fbc1bddb58545de24dd82594a415ed9af8002c
eo-uk <london.ekin@gmail.com>
2021-11-23 12:44:31 +0000

    added wordpair repository interface

--- DFESW4-TranslationAPI/src/main/java/com/qa/dfesw4translationapi/entities/WordPairRepository.java
+++ DFESW4-TranslationAPI/src/main/java/com/qa/dfesw4translationapi/entities/WordPairRepository.java
@@ -1,5 +1,10 @@
 package com.qa.dfesw4translationapi.entities;
 
-public interface WordPairRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface WordPairRepository extends JpaRepository<WordPair, Integer> {
 
 }
