dc3d786908ea79614c8ae1ba56efdf80a1f0f0c1
eo-uk <london.ekin@gmail.com>
2021-11-25 15:22:04 +0000

    connected to mysql db

--- DFESW4-TranslationAPI/src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO WORD_PAIR
-	(LANGUAGE1, LANGUAGE1WORD, LANGUAGE2, LANGUAGE2WORD, FIELD, DATE_CREATED)
-VALUES
-	('english', 'banana', 'spanish', 'platano', 'general', '2021-11-24'),
-    ('english', 'doctor', 'spanish', 'medico', 'medical', '2021-11-24'),
-    ('german', 'wagen', 'english', 'car', 'general', '2021-11-24'),
-    ('turkish', 'yalan beyan', 'english', 'perjury', 'legal', '2021-11-24'),
-    ('english', 'motherboard', 'turkish', 'anakart', 'technology', '2021-11-24');
