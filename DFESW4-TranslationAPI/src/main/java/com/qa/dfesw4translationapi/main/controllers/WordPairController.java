42d7807be40e68c36950d9c6040783f1576d6f23
eo-uk <london.ekin@gmail.com>
2021-11-25 09:56:07 +0000

    added responseentity to postmapping

--- DFESW4-TranslationAPI/src/main/java/com/qa/dfesw4translationapi/main/controllers/WordPairController.java
+++ DFESW4-TranslationAPI/src/main/java/com/qa/dfesw4translationapi/main/controllers/WordPairController.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -88,9 +90,8 @@ public class WordPairController {
 	}
 	
 	@PostMapping("/words/add")
-	public HashMap<String, String> addWord(@RequestBody WordPair word) {
-		service.createWord(word);
-		return getResponseSuccess();
+	public ResponseEntity<WordPair> addWord(@RequestBody WordPair word) {
+		return new ResponseEntity<>(service.createWord(word), HttpStatus.CREATED);
 	}
 	
 	@DeleteMapping("/words/delete/{id}")
